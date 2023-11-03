ac79a589d5a8adea1bfea5df57cf88e4897caf67
Pavelshlykov91 <pavelshlykov91@gmail.com>
2023-11-03 18:50:27 +0300

    feat:Navbar

--- client/src/features/navbar/Navbar.tsx
+++ client/src/features/navbar/Navbar.tsx
@@ -1,24 +1,33 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import './navbar.css';
 
 function Navbar() {
-    
   return (
     <>
-<div className="navbar">
-  <label htmlFor="home">Главная страница</label>
-  <label htmlFor="schedule">Расписание</label>
-  <label htmlFor="register">Регистрация</label>
-  <label htmlFor="login">Авторизация</label>
-  <label htmlFor="logout">Выход</label>
-</div>
- <Outlet />
- </>
-  )
+      <div className="navbar">
+        <Link to="/" className="navbar-link">
+          Главная страница
+        </Link>
+        <Link to="/library" className="navbar-link">
+          Библиотека
+        </Link>
+        <Link to="/gallery" className="navbar-link">
+          Галерея
+        </Link>
+        <Link to="/interview" className="navbar-link">
+          Интервью
+        </Link>
+        <Link to="/" className="navbar-link">
+          Регистрация
+        </Link>
+      </div>
+      <Outlet />
+    </>
+  );
 }
 
-export default Navbar
-
+export default Navbar;
