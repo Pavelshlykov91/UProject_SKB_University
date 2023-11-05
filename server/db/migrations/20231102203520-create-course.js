7ececb490427fa07400db024ff6a75a887d65641
Pavelshlykov91 <pavelshlykov91@gmail.com>
2023-11-05 20:21:04 +0300

    fix:BdNew

--- server/db/migrations/20231102203520-create-course.js
+++ server/db/migrations/20231102203520-create-course.js
@@ -3,6 +3,12 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
       name: {
         type: Sequelize.TEXT,
         allowNull: false
