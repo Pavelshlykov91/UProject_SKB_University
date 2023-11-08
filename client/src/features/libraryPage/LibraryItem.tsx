251059d7fa0b74757183789ee5a449f385a90e65
L1veng <i.gerasevv@gmail.com>
2023-11-08 17:55:26 +0300

    books

--- client/src/features/libraryPage/LibraryItem.tsx
+++ client/src/features/libraryPage/LibraryItem.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React  from 'react';
 import { Link } from 'react-router-dom';
 import type { Material } from './type';
 
 const LibraryItem = ({ material }: { material: Material }): JSX.Element => {
+
+
   return (
     <div>
       <Link to={`/library/${material.id}`}>
         <div className="library-item">
-          <img src={material.url} alt="не удалось загрузить" />
+          {material.url ? (
+            <img src={material.url} alt="не удалось загрузить" />
+          ) : (
+            <img src="../../../public/img/book.jpeg" alt="img" />
+          )}
+
           <div className="library-item-content">
             <div className="library-item-text">
               <h3>{material.author}</h3>
