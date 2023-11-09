19013f46e1b8d0a209de3dcfb7ab9a740aaf4c71
Pavelshlykov91 <pavelshlykov91@gmail.com>
2023-11-09 23:59:44 +0300

    feat: baseInter

--- server/db/migrations/20231102222853-create-exercise.js
+++ server/db/migrations/20231102222853-create-exercise.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const { sequelize } = require('../models');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -26,6 +29,10 @@ module.exports = {
           key: 'id'
         },
         onDelete: 'CASCADE',
+      },
+      done_status: {
+        type: sequelize.BOOLEAN,
+        allowNull: false
       },
       title: {
         type: Sequelize.TEXT,
--- server/db/models/exercise.js
+++ server/db/models/exercise.js
@@ -31,6 +31,10 @@ module.exports = (sequelize, DataTypes) => {
       },
       onDelete: 'CASCADE',
     },
+    done_status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
     title: {
       type: DataTypes.TEXT,
       allowNull: false
