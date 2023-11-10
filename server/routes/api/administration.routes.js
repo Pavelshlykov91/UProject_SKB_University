079b0eabd55df399344dbc2840b9ccf3ff2eb4c1
Pavelshlykov91 <pavelshlykov91@gmail.com>
2023-11-10 16:04:32 +0300

    fix

--- server/routes/api/administration.routes.js
+++ server/routes/api/administration.routes.js
@@ -32,6 +32,7 @@ router.get('/exercise', async (req, res) => {
         {model: Answer}
       ],
     });
+
     res.json(exercises);
   } catch ({ message }) {
     res.status(500).json({ message });
